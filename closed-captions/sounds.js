70fe28ab3fee2b9b1bdc8acbb87d86c988eb5744
BrianJakobs <31827219+BrianJakobs@users.noreply.github.com>
2019-05-21 01:29:35 +0200

    V4
    Alarm knoppen zijn toegevoegd die onderdeel van het 'dashboard' zijn. Ook zijn deze gelijk responsive gemaakt.
    
    Volgende doel is het toevoegen van de hartslag en het stoppen van het 'dashboard' wanneer de hartslag begint. Ook wil ik kijken of ik meer toe kan voegen aan het dashboard, bijvoorbeeld een constante lijn die met een golvende beweging over het beeld beweegt.

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,7 +1,22 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
+    //Door knock
 	9.109,
+    //Biep A
+    15.3,
+    //Constant B
+    36,
+    //Biep A
+    40.5,
+    //Biep A
+    70.5,
+    //Heartbeat start
     109.372,
-    145.024
+    //Heartbeat stop
+    145.024,
+    //Gunshot 1
+    268,
+    //Gunshot 2
+    272
 ];
